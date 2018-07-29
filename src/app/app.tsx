2fb527085bf42a7604c7e749dd060bdb62043bae
Dyesse YUMBA <yumba.nduba@gmail.com>
2018-07-29 18:04:35 +0100

    ✨ Feature/layout (#1)
    * 🚧 Adding side bar.
    
    * 🚧 Adding side bar
    
    * ✨ Added side bar feature.

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { SideBar } from './components';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        <SideBar />
+      </div>
+    );
+  }
+}
+
+export { App };
