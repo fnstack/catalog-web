72af87739c7007b95ee0ce0cdd92b4f80fcbe670
Dyesse YUMBA <yumba.nduba@gmail.com>
2018-07-29 19:05:34 +0100

    ✨ Added product category component. (#2)

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import { SideBar } from './components';
+import { Route, Switch } from 'react-router-dom';
+import { ProductCategories } from 'productCategories';
 
 class App extends React.Component {
   public render() {
     return (
-      <div>
+      <div style={{ display: 'flex' }}>
         <SideBar />
+        <div>
+          <Switch>
+            <Route exact path="/product-categories" component={ProductCategories} />
+          </Switch>
+        </div>
       </div>
     );
   }
---
+++ src/productCategories/ProductCategories.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+class ProductCategories extends React.Component {
+  public render() {
+    console.log('page-loaded');
+    return (
+      <div>
+        <h1>Catégorie des produits</h1>
+      </div>
+    );
+  }
+}
+
+export { ProductCategories };
