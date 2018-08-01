1e8ecb5c306db64dc481d16e577643347b9275ee
Gloire Yumba <gloireyumba@hotmail.com>
2018-08-01 17:34:01 +0100

    🤔  Added Products Component.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { SideBar } from './components';
 import { Route, Switch } from 'react-router-dom';
 import { ProductCategories } from 'productCategories';
+import { Products } from 'products';
 
 class App extends React.Component {
   public render() {
@@ -11,6 +12,7 @@ class App extends React.Component {
         <div>
           <Switch>
             <Route exact path="/product-categories" component={ProductCategories} />
+            <Route exact path="/products" component={Products} />
           </Switch>
         </div>
       </div>
