b700d03c740ac31b7c4040d342a5f31e7f44cbbb
Dyesse YUMBA <yumba.nduba@gmail.com>
2018-08-07 19:59:29 +0100

    Fixed graphql issues. (#9)

--- src/productBrands/ProductBrands.tsx
+++ src/productBrands/ProductBrands.tsx
@@ -1,57 +1,59 @@
 import * as React from 'React';
-import {Button} from 'antd';
-import {Row,Col} from 'antd';
+import { Button } from 'antd';
+import { Row, Col } from 'antd';
+import { Query } from 'react-apollo';
+import { productBrands } from 'productBrands';
 
 class ProductBrands extends React.Component {
   public render() {
-    console.log('page-loader');
     return (
+      <Query query={productBrands}>
+        {({ loading, error, data }) => {
+          if (loading) {
+            return 'Loading...';
+          } else {
+            console.log('data', data);
+          }
 
-      <div>
-        <Row>
-          <Col span={24}>
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <h1>Product Brand</h1>
-        <Button type="primary">Ajouter</Button>
-      </div>
-        <hr />
-        <div >
-          <b>Nom :</b>
-          <br />
-          <b>Description :</b>
-          <br />
-          <Button type="primary">Renomer</Button>
-          &nbsp;&nbsp;&nbsp;
-          <Button type="primary">Supprimer</Button>
+          if (error) {
+            return <span>Erreur: {error.message}</span>;
+          }
 
-        </div>
-        <hr />
-        <div>
-          <b>Nom :</b>
-          <br />
-          <b>Description :</b>
-          <br />
-          <Button type="primary">Renomer</Button>
-          &nbsp;&nbsp;&nbsp;
-          <Button type="primary">Supprimer</Button>
-          <hr />
-          </div>
-         <div>
-           <b>Nom :</b>
-           <br />
-           <b>Description</b>
-           <br />
-           <Button type="primary">Renomer</Button>
-          &nbsp;&nbsp;&nbsp;
-          <Button type="primary">Supprimer</Button>
-           <hr />
-         </div>
-         </Col>
-         </Row>
-        </div>
+          let results = [];
 
+          if (Array.isArray(data.productBrands)) {
+            results = data.productBrands.map(brand => (
+              <React.Fragment>
+                <div>
+                  <b>Nom : {brand.name}</b>
+                  <br />
+                  <b>Description :</b>
+                  <br />
+                  <Button type="primary">Renomer</Button>
+                  &nbsp;&nbsp;&nbsp;
+                  <Button type="primary">Supprimer</Button>
+                </div>
+                <hr />
+              </React.Fragment>
+            ));
+          }
 
-
+          return (
+            <div>
+              <Row>
+                <Col span={24}>
+                  <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <h1>Product Brand</h1>
+                    <Button type="primary">Ajouter</Button>
+                  </div>
+                  <hr />
+                  {results}
+                </Col>
+              </Row>
+            </div>
+          );
+        }}
+      </Query>
     );
   }
 }
--- src/productBrands/index.ts
+++ src/productBrands/index.ts
@@ -1,1 +1,2 @@
 export * from './ProductBrands';
+export * from './queries';
