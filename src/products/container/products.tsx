745e5dc809a361df1030093b42969c6ef8111c74
Gloire Yumba <33695126+gloireyumba@users.noreply.github.com>
2018-08-16 09:14:03 +0100

    Now we can see products. (#10)

--- src/products/container/products.tsx
+++ src/products/container/products.tsx
@@ -1,11 +1,53 @@
 import * as React from 'react';
 import { Button, Row, Col } from 'antd';
 import { AddNewProduct } from '../components';
+import { Query } from 'react-apollo';
+import { products } from '../queries';
 
 class Products extends React.Component {
   public render() {
     return (
-      <div>
+      <Query query={products}>
+        {({loading, error, data}) => {
+          if (loading) {
+            return 'Loading...';
+          } 
+          else {
+            console.log('data', data);
+          }
+          if (error) {
+            return <span>Erreur: {error.message}</span>;
+          }
+
+          let results = [];
+
+          if (Array.isArray(data.products)) {
+            results = data.products.map(product =>(
+              <React.Fragment>
+                <div style={{ paddingLeft: 10 }}>
+              <b>Nom: {product.name}</b>
+              <br />
+              <b>Description: {product.description}</b>
+              <br />
+              <b>Catégorie:</b>
+              <br />
+              <b>Marque:</b>
+              <br />
+              <Button style={{ marginTop: 5 }} type="primary">
+                Renommer
+              </Button>
+              &nbsp;&nbsp;&nbsp;&nbsp;
+              <Button style={{ marginTop: 5 }} type="primary">
+                Supprimer
+              </Button>
+            </div>
+            <hr />
+              </React.Fragment>
+            ));
+          }
+
+          return (         
+        <div>
         <Row>
           <Col span={24}>
             <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
@@ -13,59 +55,15 @@ class Products extends React.Component {
               <div>                
                 <AddNewProduct />
               </div>
-            </div>
+            </div> 
             <hr />
-            <div style={{ paddingLeft: 10 }}>
-              <b>Nom:</b>
-              <br />
-              <b>Description:</b>
-              <br />
-              <b>Catégorie:</b>
-              <br />
-              <Button style={{ marginTop: 5 }} type="primary">
-                Renommer
-              </Button>
-              &nbsp;&nbsp;&nbsp;&nbsp;
-              <Button style={{ marginTop: 5 }} type="primary">
-                Supprimer
-              </Button>
-            </div>
-            <hr />
-            <div style={{ paddingLeft: 10 }}>
-              <b>Nom:</b>
-              <br />
-              <b>Description:</b>
-              <br />
-              <b>Catégorie:</b>
-              <br />
-              <Button style={{ marginTop: 5 }} type="primary">
-                Renommer
-              </Button>
-              &nbsp;&nbsp;&nbsp;&nbsp;
-              <Button style={{ marginTop: 5 }} type="primary">
-                Supprimer
-              </Button>
-            </div>
-            <hr />
-            <div style={{ paddingLeft: 10 }}>
-              <b>Nom:</b>
-              <br />
-              <b>Description:</b>
-              <br />
-              <b>Catégorie:</b>
-              <br />
-              <Button style={{ marginTop: 5 }} type="primary">
-                Renommer
-              </Button>
-              &nbsp;&nbsp;&nbsp;&nbsp;
-              <Button style={{ marginTop: 5 }} type="primary">
-                Supprimer
-              </Button>
-            </div>
-            <hr />
+            {results}           
           </Col>
         </Row>
       </div>
+       );
+      }}
+      </Query>
     );
   }
 }
