6e253b15b9c2b075f282fb9071a3bd94a6a22c60
Gloire Yumba <gloireyumba@hotmail.com>
2018-09-14 10:55:21 +0100

    Now we can create product without problem.

--- src/products/mutations.ts
+++ src/products/mutations.ts
@@ -1,7 +1,7 @@
 import { gql, DocumentNode } from 'apollo-boost';
 
 export const createProduct: DocumentNode = gql`
-mutation createProduct ($name: String!, $categoryId: ID, $brandId: ID, $description: String) {
+mutation createProduct ($name: String!, $categoryId: ID!, $brandId: ID!, $description: String) {
     createProduct(input: {
         name: $name,
         categoryId: $categoryId,
